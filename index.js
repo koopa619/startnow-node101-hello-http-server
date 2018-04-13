937028c1621f26bcbc80df43d8401d1de93652e5
Justin <koopa619@gmail.com>
2018-04-12 17:26:04 -0700

    http server project

---
+++ index.js
@@ -0,0 +1,12 @@
+// write your code here
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  
+  res.end('Hello world');
+  
+});
+server.listen(8080);
+
